ecec8e939cb70df3d792bcb4da719bd2239ea10a
oddknightofcrimsongrove <148396636+oddknightofcrimsongrove@users.noreply.github.com>
2024-01-30 20:21:24 -0500

    css completed but needs work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,37 @@
-const inputEl = document.getElementById ('city-name')
-const SubmitBtn = document.getElementById('submit_btn')
-const SingleDay = document.getElementById('single_day_forecast')
-const API_key = '936b505fb7e01d09c658440bbf80ecec'
+// const inputEl = document.getElementById ('city-name')
+// const submitBtn = document.getElementById('submit_btn')
+// const SingleDay = document.getElementById('single_day_forecast')
+// const API_key = '936b505fb7e01d09c658440bbf80ecec'
 
-SubmitBtn.addEventListener('click',function(){
-    let searchValue= inputEl.value;
-    let Url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${API_key}`
+// submitBtn.addEventListener('click',function(){
+//     let searchValue= inputEl.value;
+//     let Url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${API_key}&units=imperial`
 
-    fetch(Url).then(res=>{
-        return res.json()
-    }).then(data=>{
-        console.log(data)
-        let cityName=data.name;
-        SingleDay.append(cityName)
+//     fetch(Url).then(res=>{
+//         return res.json()
+//     }).then(data=>{
+//         console.log(data)
+//         let cityName = data.name;
+//         SingleDay.append(cityName)
 
-        let temp_text=`current temp=${data.main.temp}`
-        SingleDay.append(temp_text)
-    })
-})
+//         let temp_text=`temp =${data.main.temp}`
+        
+//         SingleDay.append(temp_text)
+        
+//     });
+
+//     let urlFiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=${API_key}`;
+//     fetch(urlFiveday).then(res=>{
+//         return res.json()
+//     }).then(data=>{
+//          console.log(data)
+//          let cityName = data.name;
+//          fiveDay.append(cityName)
+ 
+//         let temp_text = `weather=${data.main.temp}`
+//         fiveDay.append(temp_text)
+//         fiveDay.append(cityName)
+ 
+//     });
+// });
+// const fiveDay = document.getElementById('five_day_forecast')
