77be93167a4c1733b2f6e402ca3c2a2b1ea9b231
oddknightofcrimsongrove <148396636+oddknightofcrimsongrove@users.noreply.github.com>
2024-01-30 23:46:36 -0500

    javascript added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,26 @@
-// const inputEl = document.getElementById ('city-name')
-// const submitBtn = document.getElementById('submit_btn')
-// const SingleDay = document.getElementById('single_day_forecast')
-// const API_key = '936b505fb7e01d09c658440bbf80ecec'
 
-// submitBtn.addEventListener('click',function(){
-//     let searchValue= inputEl.value;
-//     let Url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${API_key}&units=imperial`
+const cities = [];
+document.getElementById('submit_btn').addEventListener('click', async function () {
+    const APIKey = "936b505fb7e01d09c658440bbf80ecec";
+    const city = document.getElementById('city-name')
+    
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=${APIKey}&units=imperial`)
+    const data = await response.json();
+    //    loop through the data and change the values in the cards
+    for (i = 0; i < data.list.length - 1; i++) {
+        // 
+        if (i < 5) {
+            console.log(data.list[i + 8])
+            document.getElementsByClassName('minValues')[i].innerText = `Temp: ${data.list[i + 8].main.temp_min}`;
+            document.getElementsByClassName('humidity')[i].innerText = `humidity: ${data.list[i + 8].main.humidity}`;
+            document.getElementsByClassName("imgClass")[i].src = `https://openweathermap.org/img/wn/${data.list[i + 8].weather[0].icon}.png`
+        }
 
-//     fetch(Url).then(res=>{
-//         return res.json()
-//     }).then(data=>{
-//         console.log(data)
-//         let cityName = data.name;
-//         SingleDay.append(cityName)
 
-//         let temp_text=`temp =${data.main.temp}`
-        
-//         SingleDay.append(temp_text)
-        
-//     });
+    }
 
-//     let urlFiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=${API_key}`;
-//     fetch(urlFiveday).then(res=>{
-//         return res.json()
-//     }).then(data=>{
-//          console.log(data)
-//          let cityName = data.name;
-//          fiveDay.append(cityName)
- 
-//         let temp_text = `weather=${data.main.temp}`
-//         fiveDay.append(temp_text)
-//         fiveDay.append(cityName)
- 
-//     });
-// });
-// const fiveDay = document.getElementById('five_day_forecast')
+    cities.push(city.value);
+    localStorage.setItem('cities', JSON.stringify(cities));
+})
+
+// get cities from local storage and add it to the area
